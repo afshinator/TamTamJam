edb3c6b94337d8d8962844943f3c437e56923a1d
Afshin Mokhtari <afshinator@hotmail.com>
2018-11-06 17:07:23 -0800

    Connect to fbase for a test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import { database } from './firebase';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    database.ref().on('value', (snapshot) => {
+      console.log('data changed', snapshot.val() )
+      this.setState({
+        data: snapshot.val()
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,6 +37,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <pre>
+          { JSON.stringify(this.state.data, null, 2)}
+        </pre>
       </div>
     );
   }
